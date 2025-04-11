f2e12fd6c57ef1bd1d7083104ed0a95920a75469
Florian Bär <florianbar01@gmail.com>
2025-04-11 11:25:14 +0200

    add form to weather component to input location

--- src/components/weather/index.tsx
+++ src/components/weather/index.tsx
@@ -1,15 +1,38 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useWeatherStore } from "@/stores/weather";
 
-function Weather () {
-  const { weather, loading, error, actions: { fetchWeather } } = useWeatherStore();
+function Weather() {
+  const { 
+    weather, 
+    loading, 
+    error, 
+    actions: { 
+      fetchWeather 
+    }
+  } = useWeatherStore();
 
-  useEffect(() => {
-    fetchWeather("CapeTown");
-  }, [fetchWeather]);
+  const [enteredLocation, setEnteredLocation] = useState<string>("");
+  const [formError, setFormError] = useState<string>("");
+
+  function handleLocationChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    setFormError("");
+    setEnteredLocation(event.target.value);
+  }
+
+  function handleFormSubmit (event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    setFormError("");
+
+    if (!enteredLocation) {
+      setFormError("Please enter a location");
+      return;
+    }
+
+    fetchWeather(enteredLocation);
+  }
 
   useEffect(() => {
     console.log("Weather", weather);
@@ -18,6 +41,21 @@ function Weather () {
   return (
     <div>
       <h1>Weather</h1>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <form onSubmit={handleFormSubmit}>
+          <input 
+            type="text" 
+            placeholder="Enter a location" 
+            value={enteredLocation} 
+            onChange={handleLocationChange}
+          />
+          {formError && <p>{formError}</p>}
+          {error && <p>{error}</p>}
+        </form>
+      )}
     </div>
   );
 }
